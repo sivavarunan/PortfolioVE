cd9471171d11240ef117b1ac04e85425c2a5bf0a
sivavarunan <thevarasa.20221591@iit.ac.lk>
2026-01-25 11:33:40 +0530

    updated the image

--- src/app/components/Portfolio.tsx
+++ src/app/components/Portfolio.tsx
@@ -19,7 +19,7 @@ export default function Portfolio() {
     >
       {/* Polaroid photo at the top */}
       <motion.div
-        className="absolute left-1/2 top-0 -translate-x-1/2 -z-10"
+        className="absolute left-1/2 top-0 -translate-x-[54.5%] -z-10"
         initial={{ opacity: 0, rotate: -12, y: 60 }}
         whileInView={{ opacity: 1, y: 0 }}
         viewport={{ once: true }}
@@ -30,7 +30,7 @@ export default function Portfolio() {
           alt="Polaroid"
           width={480}
           height={600}
-          className="rotate-[-18deg] shadow-[0_40px_100px_rgba(0,0,0,0.6)] mt-35"
+          className="rotate-[12deg] shadow-[0_40px_100px_rgba(0,0,0,0.6)]  mt-35"
         />
       </motion.div>
 
@@ -41,7 +41,7 @@ export default function Portfolio() {
         <div className="relative z-10 max-w-3xl mx-auto text-center -mt-16">
           <h3 className="text-5xl font-bold mb-4 text-black">About Me</h3>
                 {/* Spacer to make photo visible */}
-      <div className="h-88"></div>
+      <div className="h-88 md:mt-25"></div>
 
           <p className="text-gray-900 bg-white/70 backdrop-blur-md rounded-2xl px-6 py-5 shadow-lg">
             I am a passionate video editor with 1 year of professional experience,
@@ -54,7 +54,7 @@ export default function Portfolio() {
 
       {/* Applications */}
       <Reveal>
-        <h4 className="text-4xl font-semibold mb-6 text-center text-black md:mt-76 mt-10">
+        <h4 className="text-4xl font-semibold mb-6 text-center text-black md:mt-36 mt-10">
           Applications I Use
         </h4>
       </Reveal>
